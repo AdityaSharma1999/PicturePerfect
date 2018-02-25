ad6b55becbe20c6273973bb623443d0f9cd968a2
Aditya Sharma <aditya.sharma.dh@gmail.com>
2018-02-25 06:57:14 -0500

    Create TensorFlow.py

---
+++ TensorFlow.py
@@ -0,0 +1,57 @@
+(base) C:\Users\sharm>conda create -n tensorflow pip python=3.5
+Solving environment: done
+
+## Package Plan ##
+
+  environment location: C:\Users\sharm\Anaconda3\envs\tensorflow
+
+  added / updated specs:
+    - pip
+    - python=3.5
+
+
+The following packages will be downloaded:
+
+    package                    |            build
+    ---------------------------|-----------------
+    pip-9.0.1                  |   py35h691316f_4         2.6 MB
+    python-3.5.4               |      h1357f44_23        18.6 MB
+    setuptools-38.5.1          |           py35_0         548 KB
+    wheel-0.30.0               |   py35h38a90bc_1          85 KB
+    certifi-2018.1.18          |           py35_0         144 KB
+    wincertstore-0.2           |   py35hfebbdb8_0          13 KB
+    ------------------------------------------------------------
+                                           Total:        22.0 MB
+
+The following NEW packages will be INSTALLED:
+
+    certifi:        2018.1.18-py35_0
+    pip:            9.0.1-py35h691316f_4
+    python:         3.5.4-h1357f44_23
+    setuptools:     38.5.1-py35_0
+    vc:             14-h0510ff6_3
+    vs2015_runtime: 14.0.25123-3
+    wheel:          0.30.0-py35h38a90bc_1
+    wincertstore:   0.2-py35hfebbdb8_0
+
+Proceed ([y]/n)? y
+
+
+Downloading and Extracting Packages
+pip 9.0.1: #################################################################################################### | 100%
+python 3.5.4: ################################################################################################# | 100%
+setuptools 38.5.1: ####################################################################################################################################################################################### | 100%
+wheel 0.30.0: ############################################################################################################################################################################################ | 100%
+certifi 2018.1.18: ####################################################################################################################################################################################### | 100%
+wincertstore 0.2: ######################################################################################################################################################################################## | 100%
+Preparing transaction: done
+Verifying transaction: done
+Executing transaction: done
+#
+# To activate this environment, use
+#
+#     $ conda activate tensorflow
+#
+# To deactivate an active environment, use
+#
+#     $ conda deactivate
